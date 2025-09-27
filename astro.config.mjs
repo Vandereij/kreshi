c0118b071ff64bb823656f82b5524104f7873caf
Stefano <stefano.dirella@gmail.com>
2025-09-27 20:45:30 +0100

    Implementation of Supabase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify/functions';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import netlify from '@astrojs/netlify/functions'
+import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
   integrations: [tailwind({ applyBaseStyles: true })],
-  server: { host: true }
-});
+  vite: {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  },
+})
