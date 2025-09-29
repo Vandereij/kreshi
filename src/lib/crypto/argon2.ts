51417c8bcde3bc84600215d8032cc95d609b0cf0
Stefano <stefano.dirella@gmail.com>
2025-09-29 21:43:03 +0100

    Completely reset codebase

--- src/lib/crypto/argon2.ts
+++
@@ -1,3 +0,0 @@
-export async function deriveWithArgon2(_password: string, _salt: Uint8Array){
-  throw new Error('Argon2 not bundled in this starter. Add argon2-browser to use.');
-}
--- src/types/astro-resolve.d.ts
+++
@@ -1,4 +0,0 @@
-// Allow Astro.resolve() in .astro frontmatter without TS errors
-declare const Astro: import('astro').AstroGlobal & {
-  resolve: (path: string) => string;
-};
